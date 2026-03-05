73be8b2da958f9ba8fe645d6489e47a35ec41924
user4396517157 <1989495993940136-user4396517157@noreply.coze.cn>
2026-03-06 06:16:59 +0800

    fix(i18n): 修复messages导入路径

--- src/i18n.ts
+++ src/i18n.ts
@@ -14,7 +14,7 @@ export default getRequestConfig(async ({ requestLocale }) => {
 
   return {
     locale,
-    messages: (await import(`../messages/${locale}.json`)).default,
+    messages: (await import(`./messages/${locale}.json`)).default,
     timeZone: 'UTC',
     now: new Date(),
   };
